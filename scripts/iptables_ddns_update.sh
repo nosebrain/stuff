d812210f4dbbe7db0e76bf353a8b865297c9fe0d
Daniel Zoller <5073757+nosebrain@users.noreply.github.com>
2017-01-06 21:25:38 +0000

    updated ddns iptables script

--- scripts/iptables_ddns_update.sh
+++ scripts/iptables_ddns_update.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
-# based on http://unix.stackexchange.com/a/91711
-# added hostname parameter and network device parameter
-
+# required packages: iptables-persistent
+# 
+# based on http://unix.stackexchange.com/questions/91701/ufw-allow-traffic-only-from-a-domain-with-dynamic-ip-address
+# adapted for multiple DDNS sources
 IPTABLES=/sbin/iptables
-HOSTNAME=$1
-NORMED_HOSTNAME=`echo $HOSTNAME | tr -cd '[[:alnum:]]'`
 LOG_BASE_PATH=/var/log/ddns_update
-LOGFILE=$LOG_BASE_PATH/$NORMED_HOSTNAME.log
+
+HOSTNAME=$1
 NETWORK_DEVICE=$2
+NORMED_HOSTNAME=`echo $HOSTNAME | tr -cd '[[:alnum:]]'`
+LOGFILE=$LOG_BASE_PATH/$NORMED_HOSTNAME.log
 
 # get the old ip address
 mkdir -p $LOG_BASE_PATH
 touch $LOGFILE
 Old_IP=$(cat $LOGFILE)
 
-Current_IP=$(host $HOSTNAME | head -n1 | cut -f4 -d' ')
+Current_IP=$(host $HOSTNAME | cut -f4 -d' ')
 DATE=`date`
 
 if [ "$Current_IP" = "$Old_IP" ] ; then
-  echo "$DATE ${HOSTNAME}: IP address has not changed"
+  echo "$DATE ${HOSTNAME}: IP address has not changed ($Old_IP)"
 else
-  # only remove the old ip if set
+  # only remove the old ip when set
   if [[ ! -z "${Old_IP// }" ]]; then
     $IPTABLES -D INPUT -i $NETWORK_DEVICE -s $Old_IP -j ACCEPT
   fi
   $IPTABLES -I INPUT -i $NETWORK_DEVICE -s $Current_IP -j ACCEPT
   echo $Current_IP > $LOGFILE
-  echo "$DATE ${HOSTNAME}: iptables have been updated (${Old_IP} -> ${Current_IP})"
+  echo "$DATE ${HOSTNAME}: iptables have been updated $Old_IP -> $Current_IP"
 fi
